4527bb86b95c64d4f0cd0e8d9ad2edf2a5d951bc
parsa <parsa.ch.1381@gmail.com>
2024-03-01 15:19:23 +0330

    now download all img in any product

--- images.py
+++ images.py
@@ -4,26 +4,61 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
-import product_list
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service('./chromedriver-win64/chromedriver.exe'))
 product_url=open('ptoduct_url.text','r')
-product_url.readlines
-driver.maximize_window()
-for images in image:
-        driver.get('https://www.digikala.com/search/category-mobile-phone/product-list/')
-        time.sleep(3)
-        parentWindow= driver.window_handles
-        print(images.get_attribute('href'))
-        all_url=re.search(r'(/product/.*?/)',str(images.get_attribute('href')))
-        # all_url=re.search(r'.*product/dkp(.+?)\/',str(images.get_attribute('href')))
+count=0
+img_counter=0
+lines=product_url.readlines()
+img_url=open('img_url.text','w')
+
+for i in lines:
+    print(i)
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[-1])
+    count+=1
+    driver.get('%s'%i)
+    time.sleep(3)
+    swap_page=driver.find_element(By.XPATH,"//div[@class['ReactModal__Content ReactModal__Content--after-open bg-neutral-000 absolute overflow-y-hidden rounded-medium styles_Modal__TsaRp']]")
+    img_gallery=swap_page.find_element(By.XPATH,"//div[@class['flex flex-wrap']]")
+    img_src=img_gallery.find_elements(By.XPATH,"//img[@class['w-full bg-neutral-000 inline-block']]")
+
+    for link in img_src:
+        src_link=link.get_attribute('src')
+        verifie_img=re.search(r'(\/\/dkstatics)',str(src_link))
+        if verifie_img != None:
+            print(src_link)
+            img_counter+=1
+            print(img_counter)
+            img_url.writelines('%s\n'%src_link)
+
+
+    driver.close()
+
+    driver.switch_to.window(driver.window_handles[0])
+    if count == 2:
+        break
+driver.quit()
+img_url.close()
+
+
+
+
+# driver.maximize_window()
+# for images in image:
+#         driver.get('https://www.digikala.com/search/category-mobile-phone/product-list/')
+#         time.sleep(3)
+#         parentWindow= driver.window_handles
+#         print(images.get_attribute('href'))
+#         all_url=re.search(r'(/product/.*?/)',str(images.get_attribute('href')))
+#         # all_url=re.search(r'.*product/dkp(.+?)\/',str(images.get_attribute('href')))
           
 
 
 
 
          
-driver.close()
+# driver.close()
 print('finish')
 
--- product_list.py
+++ product_list.py
@@ -8,7 +8,7 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=Service('./chromedriver-win64/chromedriver.exe'))
 scroll=0
-driver.maximize_window()
+#driver.maximize_window()
 
 product_url=open('ptoduct_url.text','w')
 
@@ -26,9 +26,9 @@ driver.implicitly_wait(15)
 image=driver.find_element(By.XPATH,"//div[@class['product-list_ProductList__pagesContainer__zAhrX product-list_ProductList__pagesContainer--withSidebar__17nz1 product-list_ProductList__pagesContainer--withoutSidebar__aty9j']]")
 image_22=image.find_elements(By.XPATH,"//a[@class['block cursor-pointer relative bg-neutral-000 overflow-hidden grow py-3 px-4 lg:px-2 h-full styles_VerticalProductCard--hover__ud7aD']]")
 
-
-for images in image_22 :
-     all_url=images.get_attribute('href')
+ 
+for images_88 in image_22 :
+     all_url=images_88.get_attribute('href')
      image_url=re.search(r'(/product/.*?/)',str(all_url))
      if image_url != None :
           print(image_url.group(1))
@@ -37,5 +37,6 @@ for images in image_22 :
 
 
 driver.close()
+product_url.close()
 print('finish')
 
